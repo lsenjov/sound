1e6a64d941633fdeb15ebd712fc9690b37b369bc
Logan Senjov <logansenjov@gmail.com>
2023-08-05 22:23:24 +1000

    Initial commit

---
+++ separate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Don't alter jobs, it really doesn't seem to make things faster.
+# Don't need to use a --out, it already puts it foldered under htdemucs_ft
+
+demucs "$1" --mp3 --two-stems=drums -nhtdemucs_ft
