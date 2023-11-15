e5e31647f679b893799b510c037494120c43f1a8
Logan Senjov <logansenjov@gmail.com>
2023-11-15 15:09:21 +1000

    add venv activation

--- separate.sh
+++ separate.sh
@@ -3,4 +3,5 @@
 # Don't alter jobs, it really doesn't seem to make things faster.
 # Don't need to use a --out, it already puts it foldered under htdemucs_ft
 
+source .venv/bin/activate
 demucs "$1" --mp3 --two-stems=drums -nhtdemucs_ft
